2fc6d8657ee466e02e5409793023bdfa765e6749
Camiloalv200622chj <juancamiloalvarezsarmiento22@gmail.com>
2025-12-04 22:11:25 -0500

    Update categoria.vue

--- src/views/categoria.vue
+++ src/views/categoria.vue
@@ -38,7 +38,7 @@ const mostrarModal = ref(false)
 const mensajeModal = ref("")
 
 const seleccionarCategoria = (cat) => {
-localStorage.setItem("category", cat)
+const category = localStorage.getItem("categoriaSeleccionada") || "General"
 
   mensajeModal.value = `Categoría seleccionada: ${cat}`
   mostrarModal.value = true
@@ -97,5 +97,6 @@ const cerrarModal = () => {
   box-shadow: 0 0 25px rgba(200, 120, 255, 1);
 }
 </style>
+
 
 
